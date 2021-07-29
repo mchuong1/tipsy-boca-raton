66aac10e6e50ceef8ea1e1f4d794932d356af45d
Matthew Chuong <mstchuong@yahoo.com>
2021-07-28 20:37:25 -0400

    Create Carousel for testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core';
+import Carousel from 'react-material-ui-carousel';
 
 const cloudinaryUrl = 'https://res.cloudinary.com/dgpijcm0x/image/upload/Tipsy%20Boca%20Raton';
 const whiteNailPolishUrl = `${cloudinaryUrl}/white_nail_polish`;
@@ -23,23 +24,41 @@ const useStyles = makeStyles((theme) => ({
     position: 'absolute',
     top: '0vh',
     textAlign: 'center',
-    width: '100%'
+    width: '100%',
+    height: '100%',
   },
+  carousel: {
+    height: '76%'
+  },
+  indicator: {
+    position: 'absolute',
+    bottom: 0,
+  }
 }));
 
 const Testimonials = () => {
 
   const classes = useStyles();
 
+  const items = [
+    {
+        name: "Random Name #1",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s"
+    },
+    {
+        name: "Random Name #2",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s"
+    }
+  ]
+
   return (
     <div id="testimonials-container" className={classes.root}>
       <div className={classes.parallaxWhiteNailPolish} />
       <div className={classes.testimonialMsg}>
         <h1>Testimonials</h1>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-          Lorem Ipsum has been the industrys standard dummy text ever since the 1500s
-        </p>
+        <Carousel classes={{root: classes.carousel, indicators: classes.indicator}}>
+          {items.map((item) => (<div>{item.description}</div>))}
+        </Carousel>
       </div>
     </div>
   )
